2faeec51ebc63938b0518369b8f5c303ba836de8
EmmaG2 <itsmeemmadev@gmail.com>
2025-10-05 01:47:47 -0600

    refactor(plugins): organize and clean plugin configuration
    git plugins, and code formatter plugins was added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,23 +1,65 @@
 vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
 require("lazy").setup({
-  -- PLUGINS --
-  { "nvim-treesitter/nvim-treesitter",  build = ":TSUpdate" },
-  { "catppuccin/nvim",                  name = "catppuccin",                             priority = 1000 },
-  { "nvim-tree/nvim-tree.lua",          dependencies = "nvim-tree/nvim-web-devicons",    config = true },
+
+  ---------------------------------------------------------------------
+  -- 🌈 Temas y Apariencia
+  ---------------------------------------------------------------------
+  { "catppuccin/nvim",                     name = "catppuccin", priority = 999 },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl",        opts = {} },
+  { "RRethy/vim-illuminate" },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup()
+    end,
+  },
   { "goolord/alpha-nvim",               dependencies = { "nvim-tree/nvim-web-devicons" } },
-  -- Autocompletado
-  { "hrsh7th/nvim-cmp" },     -- motor de completado
-  { "hrsh7th/cmp-nvim-lsp" }, -- LSP completions
-  { "hrsh7th/cmp-buffer" },   -- sugerencias del buffer
-  { "hrsh7th/cmp-path" },     -- rutas de archivos
-  { "hrsh7th/cmp-cmdline" },  -- comandos
+
+  ---------------------------------------------------------------------
+  -- 🗂️ Navegación y Exploración
+  ---------------------------------------------------------------------
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = true,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  ---------------------------------------------------------------------
+  -- 🧠 Sintaxis y Análisis de Código
+  ---------------------------------------------------------------------
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  ---------------------------------------------------------------------
+  -- ⚙️ LSP y Autocompletado
+  ---------------------------------------------------------------------
   { "neovim/nvim-lspconfig" },
   { "williamboman/mason.nvim",          config = true },
   { "williamboman/mason-lspconfig.nvim" },
-  { "windwp/nvim-autopairs",            config = true },
-  { "nvim-telescope/telescope.nvim",    dependencies = { "nvim-lua/plenary.nvim" } },
-  { "nvim-lualine/lualine.nvim",        dependencies = { "nvim-tree/nvim-web-devicons" } },
+
+  -- Autocompletado
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "saadparwaiz1/cmp_luasnip" },
+
   -- Snippets
   {
     "L3MON4D3/LuaSnip",
@@ -25,21 +67,30 @@ require("lazy").setup({
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
       local luasnip = require("luasnip")
-
-      -- Carga los snippets en formato VSCode
       require("luasnip.loaders.from_vscode").lazy_load()
-
-      -- Opcional: recarga cuando edites tus propios snippets
-      require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnip/snippets" })
+      require("luasnip.loaders.from_lua").lazy_load({
+        paths = "~/.config/nvim/LuaSnip/snippets",
+      })
     end,
   },
-  { "saadparwaiz1/cmp_luasnip" },     -- integración
-  { "rafamadriz/friendly-snippets" }, -- snippets ya hechos (React, C++, etc.)
+  { "rafamadriz/friendly-snippets" },
+  { "windwp/nvim-autopairs",       config = true },
+
+  ---------------------------------------------------------------------
+  -- 🧹 Formateo y Limpieza. Nota: La configuración está en
+  -- /config/confom.lua
+  ---------------------------------------------------------------------
   {
-    "folke/noice.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+    "stevearc/conform.nvim",
     config = function()
-      require("noice").setup()
+      require("config.conform")
     end,
-  }
+  },
+
+  ---------------------------------------------------------------------
+  -- 🔧 Control de Versiones
+  ---------------------------------------------------------------------
+  { "lewis6991/gitsigns.nvim", config = true },
+  { "tpope/vim-fugitive" },
+
 })
