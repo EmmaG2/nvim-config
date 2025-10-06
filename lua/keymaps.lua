0e0dd08202333327cb671afe601e2f185dde395b
EmmaG2 <itsmeemmadev@gmail.com>
2025-10-05 23:47:55 -0600

    feat(keymaps): new keymaps for errors and warnings were added
    n

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -21,23 +21,27 @@ vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true, silent = true }) --
 -- === VENTANAS (splits) ===
 vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true }) -- split vertical
 vim.keymap.set('n', '<leader>s', ':split<CR>', { noremap = true, silent = true })  -- split horizontal
-vim.keymap.set('n', '<C-q>', '<C-w>h', { noremap = true, silent = true }) -- mover ventana izq
-vim.keymap.set('n', '<C-w>', '<C-w>l', { noremap = true, silent = true }) -- mover ventana der
-vim.keymap.set('n', '<C-e>', '<C-w>j', { noremap = true, silent = true }) -- mover ventana abajo
-vim.keymap.set('n', '<C-r>', '<C-w>k', { noremap = true, silent = true }) -- mover ventana arriba
+vim.keymap.set('n', '<C-q>', '<C-w>h', { noremap = true, silent = true })          -- mover ventana izq
+vim.keymap.set('n', '<C-w>', '<C-w>l', { noremap = true, silent = true })          -- mover ventana der
+vim.keymap.set('n', '<C-e>', '<C-w>j', { noremap = true, silent = true })          -- mover ventana abajo
+vim.keymap.set('n', '<C-r>', '<C-w>k', { noremap = true, silent = true })          -- mover ventana arriba
 
 -- === UTILIDAD ===
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true }) -- explorador
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })       -- explorador
 vim.keymap.set('n', '<leader>f', ':Telescope find_files<CR>', { noremap = true, silent = true }) -- buscar archivo
 vim.keymap.set('n', '<leader>g', ':Telescope live_grep<CR>', { noremap = true, silent = true })  -- buscar en texto
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { noremap = true, silent = true })  -- ver errores
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { noremap = true, silent = true })   -- ver errores
+-- Siguiente error
+vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next, { noremap = true, silent = true, desc = "Siguiente error" })
+
+-- Error anterior
+vim.keymap.set("n", "<leader>p", vim.diagnostic.goto_prev, { noremap = true, silent = true, desc = "Error anterior" })
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { noremap = true, silent = true }) -- renombrar símbolo
 
 -- === EXTRA ===
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true }) -- escape rápido
 
--- === FUZZY FINDER === 
+-- === FUZZY FINDER ===
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
-
