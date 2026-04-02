88fd5037d8086925623c9b060cec0e2d53db9087
EmmaG2 <itsmeemmadev@gmail.com>
2026-04-02 03:23:12 -0600

    feat(conform): add Prettier for web filetypes
    - Add JS/TS/JSX/TSX, Vue, Astro, HTML, CSS, JSON, YAML, Markdown
    - Fix clang-format formatter name (clang_format)
    - Set default Prettier options (single quotes, 2 spaces, es5 trailing commas)
    - Change format keymap from <leader>f to <leader>cf to avoid Telescope conflict

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -2,19 +2,49 @@ local conform = require("conform")
 
 conform.setup({
   formatters_by_ft = {
-    cpp = { "clang-format" },
-    c = { "clang-format" },
-    lua = { "stylua" },
+    -- Sistemas
+    cpp    = { "clang_format" },
+    c      = { "clang_format" },
+    lua    = { "stylua" },
     python = { "black" },
+
+    -- Web (Prettier maneja todo)
+    javascript      = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript      = { "prettier" },
+    typescriptreact = { "prettier" },
+    vue             = { "prettier" },
+    astro           = { "prettier" },
+    html            = { "prettier" },
+    css             = { "prettier" },
+    scss            = { "prettier" },
+    less            = { "prettier" },
+    json            = { "prettier" },
+    jsonc           = { "prettier" },
+    yaml            = { "prettier" },
+    markdown        = { "prettier" },
   },
+
   format_on_save = {
-    timeout_ms = 500,
+    timeout_ms   = 1000,
     lsp_fallback = true,
+  },
+
+  -- Opciones de prettier (puedes sobreescribir con .prettierrc en el proyecto)
+  formatters = {
+    prettier = {
+      prepend_args = {
+        "--single-quote",
+        "--semi",
+        "--tab-width", "2",
+        "--print-width", "100",
+        "--trailing-comma", "es5",
+      },
+    },
   },
 })
 
--- Atajo manual: <leader>f para formatear
-vim.keymap.set({ "n", "v" }, "<leader>f", function()
+-- Atajo manual: <leader>cf para formatear (evita conflicto con <leader>f de telescope)
+vim.keymap.set({ "n", "v" }, "<leader>cf", function()
   require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format file" })
-
