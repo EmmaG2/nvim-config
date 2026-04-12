ed1ca2886ded6d38df481744f09b034e8e860aae
EmmaG2 <itsmeemmadev@gmail.com>
2026-04-11 23:05:01 -0600

    fix(theme): move colorscheme call after catppuccin setup

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,14 +1,14 @@
-vim.cmd.colorscheme "catppuccin"
-
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    transparent_background = false,
-    integrations = {
-        treesitter = true,
-        native_lsp = { enabled = true },
-        cmp = true,
-        gitsigns = true,
-        telescope = true,
-        nvimtree = true,
-    }
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  transparent_background = false,
+  integrations = {
+    treesitter = true,
+    native_lsp = { enabled = true },
+    cmp = true,
+    gitsigns = true,
+    telescope = true,
+    nvimtree = true,
+  }
 })
+
+vim.cmd.colorscheme "catppuccin"
